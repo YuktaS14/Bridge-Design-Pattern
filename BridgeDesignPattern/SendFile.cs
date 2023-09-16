87645fe7a73e1753a38eb12b294e6644ddc93b73
Yukta Salunkhe <85993659+YuktaS14@users.noreply.github.com>
2023-09-16 23:42:07 +0530

    Added Commits

--- BridgeDesignPattern/SendFile.cs
+++ BridgeDesignPattern/SendFile.cs
@@ -23,11 +23,22 @@ namespace BridgeDesignPattern
     /// </summary>
     public class SendFile : Features
     {  
+        /// <summary>
+        /// _fileName stores the name of the file, which user wants to send.
+        /// </summary>
         private readonly string _fileName;
 
+        /// <summary>
+        /// SendFile class can be used to implement different functionalities needed for sending a file from different applications. 
+        /// </summary>
         public SendFile(string file) {
             _fileName = file;
         }
+
+        /// <summary>
+        /// By default the Application used would be WhatsApp.
+        /// Work() is overriden to use the SendFile feature.
+        /// </summary>
         public override string Work()
         {
             _Application ??= new WhatsApp();
