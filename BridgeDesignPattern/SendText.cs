3c440af06e433e653bf65e2cb843f7a5ab7e3386
Yukta Salunkhe <85993659+YuktaS14@users.noreply.github.com>
2023-09-16 23:41:50 +0530

    Added Changes

--- BridgeDesignPattern/SendText.cs
+++ BridgeDesignPattern/SendText.cs
@@ -22,11 +22,22 @@ namespace BridgeDesignPattern
     /// </summary>
     public class SendText : Features
     {
+        /// <summary>
+        /// _text stores the message which user wants to send.
+        /// </summary>
         private readonly string _text;
-     
+
+        /// <summary>
+        /// SendText class can be used to implement different functionalities needed for sending text. 
+        /// </summary>
         public SendText(string text) {
             _text = text; 
         }
+        
+        /// <summary>
+        /// By default the Application used would be WhatsApp.
+        /// Work() is overriden to use the SendText feature.
+        /// </summary>
         public override string Work()
         {
             _Application ??=  new WhatsApp();
